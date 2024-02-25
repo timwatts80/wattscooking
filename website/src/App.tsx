a90066d65c7fdeb9f0de0e75e4d48d0cf12480a2
Timmy <tim_watts@ymail.com>
2024-02-25 10:47:14 -0700

    update to home page

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+"use client";
+import React, { useState } from 'react';
+import { Box, Button, Container } from "@mui/material";
+import LightbulbOutlinedIcon from '@mui/icons-material/LightbulbOutlined';
 
-function App() {
+import './global.css';
+
+export default function Home() {
+  const [buttonLabel, setButtonLabel] = useState("this page is under construction.");
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm" sx={{ height: '100vh', margin: 'auto', transition: '0.3s', }}>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100%',
+        }}>
+        <div style={{ position: 'relative', display: 'inline-block', width: '100%', }}>
+          {isHovered &&
+            <LightbulbOutlinedIcon
+              sx={{
+                position: 'absolute',
+                top: '-70px',
+                left: '50%',
+                scale: '3',
+                color: 'white',
+                transform: 'translateX(-20%)'
+              }}
+            />
+          }
+          <Button
+            variant="outlined"
+            onMouseEnter={() => {
+              setButtonLabel("Please come back later.");
+              setIsHovered(true);
+            }}
+            onMouseLeave={() => {
+              setButtonLabel("this site is under construction.");
+              setIsHovered(false);
+            }}
+            sx={{
+              width: '100%',
+              padding: '15px',
+              color: 'white',
+              border: '1px solid white',
+              transition: '0.3s',
+              '&:hover': {
+                border: '1px solid white',
+                backgroundColor: 'rgba(255, 255, 255, 0.15)',
+                padding: '30px',
+              }
+            }}
+          >
+            {buttonLabel}
+          </Button>
+        </div>
+      </Box>
+    </Container>
   );
 }
-
-export default App;
